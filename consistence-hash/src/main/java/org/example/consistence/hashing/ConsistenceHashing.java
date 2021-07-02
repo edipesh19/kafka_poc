7af63bb2fbf70573c37bee4daf4db54c45d1b96d
Dipesh Dutta <dipesh.dutta@oracle.com>
2021-07-02 15:17:59 +0530

    Implement test

--- consistence-hash/src/main/java/org/example/consistence/hashing/ConsistenceHashing.java
+++ consistence-hash/src/main/java/org/example/consistence/hashing/ConsistenceHashing.java
@@ -3,14 +3,16 @@ package org.example.consistence.hashing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.IntStream;
 
-// Todo : Implement test for scale-out and scale-in scenarios.
 public class ConsistenceHashing {
 
-    private static final int virtualNodeNum = 3;
+    private static final int virtualNodeNum = 100;
     Logger logger = LoggerFactory.getLogger(ConsistenceHashing.class);
     //key represents the hash value of the server and value represents the server
     private final SortedMap<Integer, String> sortedMap = new TreeMap<Integer, String>();
@@ -21,6 +23,7 @@ public class ConsistenceHashing {
             IntStream.range(0, virtualNodeNum)
                 .mapToObj(i -> data + "&&VN" + i).forEach(vn -> {
                 int hash = getHash(vn);
+                logger.debug("{} -> {} -- Hash {}", data, vn, hash);
                 sortedMap.put(hash, vn);
             });
             logger.info("Server [{}] added to the map", data);
@@ -40,11 +43,15 @@ public class ConsistenceHashing {
             if (subMap.isEmpty()) {
                 //If there is no one larger than the hash value of the key, start with the first node
                 Integer i = sortedMap.firstKey();
-                ret = sortedMap.get(i);
+                String serverNode = sortedMap.get(i);
+                ret = serverNode.split("&&")[0];
+                logger.debug("If | {} fall in {} server, virtual server is {}", key, serverNode, ret);
             } else {
                 //The first Key is the nearest node clockwise past the node.
                 Integer i = subMap.firstKey();
-                ret = subMap.get(i);
+                String serverNode = subMap.get(i);
+                ret = serverNode.split("&&")[0];
+                logger.debug("Else | {} fall in {} server, virtual server is {}", key, serverNode, ret);
             }
         } catch (Exception ex) {
             logger.error("Caught exception while retrieving {} from map because {}", key, ex.getMessage(), ex);
@@ -55,7 +62,16 @@ public class ConsistenceHashing {
     public String remove(String data) {
         String ret = null;
         try {
-            ret = sortedMap.remove(getHash(data));
+            Iterator<Entry<Integer, String>> iterator = sortedMap.entrySet().iterator();
+            while (iterator.hasNext())  {
+                Map.Entry<Integer, String> entry = iterator.next();
+
+                String vn = entry.getValue();
+                if(vn.contains(data)) {
+                    logger.debug("Removing values {}", vn);
+                    iterator.remove();
+                }
+            }
         } catch (Exception e) {
             logger.error("Caught exception while removing entry {} from map", data);
         }
